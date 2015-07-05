bd0694ed4e3243afb7c728718c7bb99d4662e43a
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-07-05 00:11:04 -0400

    Formatting

--- components/guest-list.ts
+++ components/guest-list.ts
@@ -1,15 +1,15 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
+
 import {Component, NgFor, View} from 'angular2/angular2';
 import {GuestCard} from 'components/guest-card';
 import {GuestService} from 'components/guest-service';
 
 @Component({
-  selector: 'guest-list',
-  directives: [NgFor, GuestCard]
+  selector: 'guest-list'
 })
 @View({
   templateUrl: 'templates/guest-list.html',
-  directives: [NgFor, GuestCard]
+  directives: [GuestCard, NgFor]
 })
 export class GuestList {
   counter: number;
