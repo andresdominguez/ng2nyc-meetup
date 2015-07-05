e8039e097267b0d69eaa08cdd85c3ce1d86a34b1
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-07-04 21:24:33 -0400

    Read data

--- components/guest-service.ts
+++ components/guest-service.ts
@@ -4,9 +4,20 @@ const FIREBASE_URL = 'https://sweltering-heat-3358.firebaseio.com';
 
 export class GuestService {
   firebase: Firebase;
+  guestList: Object[];
 
   constructor() {
     this.firebase = new Firebase(FIREBASE_URL);
+    this.guestList = [];
+
+    this.firebase.on('child_added',
+        snapshot => {
+          console.log('Got stuff');
+          var guest = snapshot.val();
+          this.guestList.push(guest);
+          console.log(guest);
+        },
+        errorObject => console.log('The read failed', errorObject.code));
   }
 
   add(name: string) {
