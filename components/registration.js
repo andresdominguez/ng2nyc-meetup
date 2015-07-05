f0540d46d7611637bb812fcb2a27a075408e0ddc
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-07-04 23:57:46 -0400

    Navigate to list on submit

--- components/registration.js
+++ components/registration.js
@@ -12,12 +12,20 @@ if (typeof __metadata !== "function") __metadata = function (k, v) {
 /// <reference path="../typings/angular2/angular2.d.ts" />
 var angular2_1 = require('angular2/angular2');
 var guest_service_1 = require('components/guest-service');
+var router_1 = require('angular2/router');
 var Registration = (function () {
-    function Registration(guestService) {
+    function Registration(guestService, router) {
         this.guestService = guestService;
+        this.router = router;
     }
-    Registration.prototype.addGuest = function (name, about) {
+    Registration.prototype.addGuest = function (event, name, about) {
+        event.preventDefault();
         this.guestService.add(name, about);
+        this.router.parent.navigate('/list').then(function (aa) {
+            debugger;
+        }, function (bb) {
+            debugger;
+        });
     };
     Registration = __decorate([
         angular2_1.Component({
@@ -26,7 +34,7 @@ var Registration = (function () {
         angular2_1.View({
             templateUrl: 'templates/registration.html'
         }), 
-        __metadata('design:paramtypes', [guest_service_1.GuestService])
+        __metadata('design:paramtypes', [guest_service_1.GuestService, router_1.Router])
     ], Registration);
     return Registration;
 })();
--- components/registration.ts
+++ components/registration.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
 import {Component, View} from 'angular2/angular2';
 import {GuestService} from 'components/guest-service';
+import {Router} from 'angular2/router';
 
 @Component({
   selector: 'registration'
@@ -10,12 +11,21 @@ import {GuestService} from 'components/guest-service';
 })
 export class Registration {
   guestService: GuestService;
+  router: Router;
 
-  constructor(guestService: GuestService) {
+  constructor(guestService: GuestService, router: Router) {
     this.guestService = guestService;
+    this.router = router;
   }
 
-  addGuest(name: string, about: string) {
+  addGuest(event, name: string, about: string) {
+    event.preventDefault();
+
     this.guestService.add(name, about);
+    this.router.parent.navigate('/list').then(function(aa) {
+      debugger;
+    }, function (bb) {
+      debugger;
+    })
   }
 }
